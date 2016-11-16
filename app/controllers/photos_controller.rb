ce68a36b73ce10bdeffbc6ffb5544c94e2a0e98f
sofiamatulis <sosomatulis@hotmail.com>
2016-11-16 00:35:19 -0500

    authentication works

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,9 @@
+class PhotosController < ApplicationController
+
+  def callback
+    response = Instagram.get_access_token(params[:code], redirect_uri: ENV['REDIRECT_URI'])
+    session[:access_token] = response.access_token
+    redirect_to root_path
+  end
+
+end

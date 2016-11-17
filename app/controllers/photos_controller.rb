d70c7c0e325df3298155ea0d918f328adf7db43a
sofiamatulis <sosomatulis@hotmail.com>
2016-11-17 16:06:31 -0500

    search button working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,12 @@
 class PhotosController < ApplicationController
 
-#
   def index
-    client = Instagram.client(:access_token => ENV["ACCESS_TOKEN"])
-   @pictures = client.get('/v1/tags/icecream/media/recent')
-  # render json: pictures
+    # saving the token in the client
+  client = Instagram.client(:access_token => ENV["ACCESS_TOKEN"])
+# if there is no tag, use icecream tag!
+  @tag_name =  params[:tag_name] ||= 'icecream'
+# this is the end point to look for these photos
+   @pictures = client.get("tags/#{@tag_name}/media/recent")
   end
 
 end

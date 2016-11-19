3df6578ae44a83ac36162dff02054693c9e52ba6
sofiamatulis <sosomatulis@hotmail.com>
2016-11-19 11:41:48 -0500

    created method to pull photos by date

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,29 @@
 class PhotosController < ApplicationController
 
+  helper_method :pull_time
+
   def index
     # saving the token in the client
   client = Instagram.client(:access_token => ENV["ACCESS_TOKEN"])
-# if there is no tag, use icecream tag!
+# if there is no tag, use icecream tag! if there is no date, use this random date
   @tag_name =  params[:tag_name] ||= 'icecream'
+  @start_date = params[:start_date] ||= "1479571118"
+  @end_date = params[:end_date] ||= "1479571117"
 # this is the end point to look for these photos
    @pictures = client.get("tags/#{@tag_name}/media/recent")
+ # render json: pull_time
+ # render json: @pictures
+
+  end
+
+  private
+  def pull_time
+    @pictures.each do |picture|
+      if @start_date >= picture.created_time && @end_date <= picture.created_time
+      return picture
+       puts picture.created_time
+      end
+    end
   end
 
 end
--- db/migrate/20161118200716_create_users.rb
+++ db/migrate/20161118200716_create_users.rb
@@ -2,9 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :client_id
-      t.string :client_secret
-      t.string :client_token
+      t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
